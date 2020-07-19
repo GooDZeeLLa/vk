fa07c98a63dc8ab9e34855cfea32b5d94ae68f70
GooDZeeLLa <68479665+GooDZeeLLa@users.noreply.github.com>
2020-07-19 23:15:13 +0300

    Updates

---
+++ precache-manifest.4a4754de7ce991cf53840aedd0c38c2c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1501f8a213b6f384b345a5187eb445",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c74d38118873b5ff549a",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "e3319188d5f377e977cd",
+    "url": "./static/css/main.1cf9cea2.chunk.css"
+  },
+  {
+    "revision": "c74d38118873b5ff549a",
+    "url": "./static/js/2.6cffc37d.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.6cffc37d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3319188d5f377e977cd",
+    "url": "./static/js/main.6ca79f05.chunk.js"
+  },
+  {
+    "revision": "bea8310ac82d8f18b365",
+    "url": "./static/js/runtime-main.c9a1e25b.js"
+  },
+  {
+    "revision": "e58d391f0459aceb6c290293d42bae1b",
+    "url": "./static/media/i1.e58d391f.jpg"
+  },
+  {
+    "revision": "1ea44d2d3d8f5507dd78f5765bb8a1bd",
+    "url": "./static/media/i2.1ea44d2d.jpg"
+  },
+  {
+    "revision": "1629a8c7a50669b19a0c401e8997bf38",
+    "url": "./static/media/img8.1629a8c7.jpg"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2fe98603b36b06791ac138163cc33154.js"
+  "./precache-manifest.4a4754de7ce991cf53840aedd0c38c2c.js"
 );
 
 self.addEventListener('message', (event) => {
